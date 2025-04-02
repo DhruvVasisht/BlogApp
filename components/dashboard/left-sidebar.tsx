526d4180b633290a70ebb8f35ba435e090873fa8
DhruvVasisht <vasisht.dhruv7@gmail.com>
2025-04-02 11:04:03 +0530

    Added Sidebar Buttons

--- components/dashboard/left-sidebar.tsx
+++ components/dashboard/left-sidebar.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { LayoutDashboard } from "lucide-react";
+import { BarChart, FileText, LayoutDashboard, MessageCircle, Settings } from "lucide-react";
 import Link from "next/link";
 
 const LeftSidebar = () => {
@@ -8,7 +8,7 @@ const LeftSidebar = () => {
     <div>
       <Sheet>
         <SheetTrigger asChild>
-          <Button>
+          <Button variant={'outline'} className="md:hidden m-4 ">
             <LayoutDashboard className="h-5 w-5" />
           </Button>
         </SheetTrigger>
@@ -30,6 +30,40 @@ const DashboardSidebar = () => {
           <span>HashByte</span>
         </Link>
       </div>
+      <nav>
+        <Link href='/dashboard'>
+        <Button variant={'ghost'} className="w-full justify-start ">
+          <LayoutDashboard className="w-5 h-5 mr-2 "/>
+          Overview
+        </Button>
+        </Link>
+        <Link href='/articles'>
+        <Button variant={'ghost'} className="w-full justify-start ">
+          <FileText className="w-5 h-5 mr-2 "/>
+          Articles
+        </Button>
+        </Link>
+        <Link href='/comments'>
+        <Button variant={'ghost'} className="w-full justify-start ">
+          <MessageCircle className="w-5 h-5 mr-2 "/>
+          Comments
+        </Button>
+        </Link>
+        <Link href='/anayltics'>
+        <Button variant={'ghost'} className="w-full justify-start ">
+          <BarChart className="w-5 h-5 mr-2 "/>
+          Analytics
+        </Button>
+        </Link>
+        <Link href='/settings'>
+        <Button variant={'ghost'} className="w-full justify-start ">
+          <Settings className="w-5 h-5 mr-2 "/>
+          Settings
+        </Button>
+        </Link>
+
+      </nav>
     </div>
   );
 };
+
