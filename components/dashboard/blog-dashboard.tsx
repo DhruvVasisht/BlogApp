203b675dfd1dfe69fa1cff9af9068f8fec35f3da
DhruvVasisht <vasisht.dhruv7@gmail.com>
2025-04-03 15:01:56 +0530

    Added New Updates

--- components/dashboard/blog-dashboard.tsx
+++ components/dashboard/blog-dashboard.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
+import { PlusCircle } from "lucide-react";
 
 const BlogDashboard = () => {
-  return <div>BlogDashboard</div>;
+  return (
+    <main className="flex-1 p-4 md:p-8">
+      <div className="flex justify-between items-center mb-8">
+        <div>
+          <h1 className="font-bold text-2xl">Blog Dashboard</h1>
+          <p>Manage your content & analytics</p>
+        </div>
+        <Link href="/dashboard/articles/create">
+        <Button>New Article <PlusCircle className="h-4 w-4"/></Button>
+         
+          
+        </Link>
+      </div>
+    </main>
+  );
 };
 
 export default BlogDashboard;
--- components/dashboard/left-sidebar.tsx
+++ components/dashboard/left-sidebar.tsx
@@ -1,21 +1,27 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { BarChart, FileText, LayoutDashboard, MessageCircle, Settings } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 const LeftSidebar = () => {
+  const [isOpen, setisOpen] = useState(false);
   return (
     <div>
-      <Sheet>
+      <Sheet open={isOpen} onOpenChange={setisOpen}>
         <SheetTrigger asChild>
           <Button variant={'outline'} className="md:hidden m-4 ">
             <LayoutDashboard className="h-5 w-5" />
           </Button>
         </SheetTrigger>
-        <SheetContent>
+        <SheetContent side={'left'} className="w-[250px]">
           <DashboardSidebar />
         </SheetContent>
       </Sheet>
+      <div className="hidden md:block h-screen w-[250px] border-r bg-background">
+        <DashboardSidebar />
+      </div>
     </div>
   );
 };
@@ -27,7 +33,7 @@ const DashboardSidebar = () => {
     <div className="h-full px-4 py-6">
       <div className="flex items-center gap-2 mb-8 px-2">
         <Link href={"/"}>
-          <span>HashByte</span>
+          <span className="text-xl">HashByte</span>
         </Link>
       </div>
       <nav>
